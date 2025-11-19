8396ca1f2bd592e4849366a8e43792fd3a88ba30
Nightingale42 <89876698+Nightingale42@users.noreply.github.com>
2025-11-18 22:03:03 -0500

    fixed enemy movement
    fixed the enemy's movement and collisions + player mouse sensitivity + Tree LOD adjustments

---
+++ Assets/Scripts/CollectibleCount.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollectibleCount : MonoBehaviour
+{
+    TMPro.TMP_Text text;
+    public int count;
+    public WinLose winLoseScript;
+
+    void Awake()
+    {
+        text = GetComponent<TMPro.TMP_Text>();
+    }
+
+    void Start() => UpdateCount();
+
+    void OnEnable() => Collectible.OnCollected += OnCollectibleCollected;
+    void onDisable()=> Collectible.OnCollected -= OnCollectibleCollected;
+
+    void OnCollectibleCollected()
+    {
+        ++count;
+        UpdateCount();
+
+        if (count > 0)
+        {
+            Debug.Log("you winny");
+            SceneManager.LoadScene("GameWin");
+
+        }
+
+
+    }
+
+    void UpdateCount()
+    {
+        text.text = $"{count} / {Collectible.total}";
+    }
+}
+
