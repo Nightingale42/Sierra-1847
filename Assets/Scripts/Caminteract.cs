0ad75241856a9ea6e74004903b7c95a75feb7634
Nightingale42 <89876698+Nightingale42@users.noreply.github.com>
2025-11-23 01:37:38 -0500

    Finished Dialogue w options
    implemented dialogue with options without paths. Added in tracking for zoom and smooth transitions for zoom

--- Assets/Scripts/Caminteract.cs
+++ Assets/Scripts/Caminteract.cs
@@ -6,6 +6,8 @@ using Cinemachine;
 
 public class Caminteract : MonoBehaviour
 {
+    public LookAtFunction LookAtScript;
+
     public Text InteractionText;
 
     public bool CanInteract = true;
@@ -15,10 +17,21 @@ public class Caminteract : MonoBehaviour
     public CinemachineVirtualCamera PlayerVcam;
     public CinemachineVirtualCamera TalkZoomVcam;
 
-    public PlayerMovement FpsConroller;
+    public PlayerMovement FpsController;
+
+    //talk
+        public GameObject TalkPanel; 
+
+        public GameObject ChoicePack;
+
+        public Text SubText;
+        string holder;
+        float time = 0.05f;
+
+    //talk
 
 
-    private float InteractDistance = 5f;
+    private float InteractDistance = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -67,13 +80,70 @@ public class Caminteract : MonoBehaviour
     IEnumerator TalkToFriendCO()
     {
         InteractionText.text = "";
-        FpsConroller.enabled = false;
+        FpsController.enabled = false;
         TalkZoomVcam.enabled = true;
         PlayerVcam.enabled = false;
 
+        //look at
+        LookAtScript.IKActive = true;
+        //look at
+
+        yield return new WaitForSeconds(1f);
+
+        FpsController.enabled = false;
+
+
+        //cursor
+         Cursor.lockState = CursorLockMode.None;
+         Cursor.visible = true;
+        //cursor
+
+        TalkPanel.SetActive(true);
+
+        SubText.text = "Me: ";
+        holder = "Hellur sister period queen";
+        foreach(char c in holder)
+        {
+            SubText.text += c;
+            yield return new WaitForSeconds(time);
+
+        }
+
+        yield return MousePress();
+
+              SubText.text = "Friend: ";
+        holder = "HELLLLUURRRRRR";
+        foreach(char c in holder)
+        {
+            SubText.text += c;
+            yield return new WaitForSeconds(time);
+
+        }
+        yield return MousePress();
+
+              SubText.text = "Friend: ";
+        holder = "Do you want to go to the clurb";
+        foreach(char c in holder)
+        {
+            SubText.text += c;
+            yield return new WaitForSeconds(time);
+
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        ChoicePack.SetActive(true);
+
+
+
         yield return new WaitForSeconds(5f);
 
-        FpsConroller.enabled = true;
+        //look at 
+
+        LookAtScript.IKActive = false;
+        //look at
+
+        FpsController.enabled = true;
         PlayerVcam.enabled = true;
         TalkZoomVcam.enabled = false;
 
@@ -81,4 +151,90 @@ public class Caminteract : MonoBehaviour
 
 
     }
+    
+    public void Choice1Void()
+    {
+        StartCoroutine(Choice1CO());
+    }
+
+    public void Choice2Void()
+    {
+
+        StartCoroutine(Choice2CO());
+
+    }
+
+
+    IEnumerator Choice1CO()
+    {
+        ChoicePack.SetActive(false);
+
+         SubText.text = "Me: ";
+        holder = "Yes";
+        foreach(char c in holder)
+        {
+            SubText.text += c;
+            yield return new WaitForSeconds(time);
+
+        }
+
+   StartCoroutine(FinalCO());
+    }
+
+     IEnumerator Choice2CO()
+    {
+
+        ChoicePack.SetActive(false);
+
+            SubText.text = "Me: ";
+        holder = "No";
+        foreach(char c in holder)
+        {
+            SubText.text += c;
+            yield return new WaitForSeconds(time);
+
+        }
+
+      StartCoroutine(FinalCO());
+
+        
+
+
+        }
+
+        IEnumerator FinalCO()
+        {
+            TalkPanel.SetActive(false);
+            FpsController.enabled = true;
+            ChoicePack.SetActive(false);
+            SubText.text = "";
+            //look at 
+
+        LookAtScript.IKActive = false;
+        //look at
+
+        FpsController.enabled = true;
+        PlayerVcam.enabled = true;
+        TalkZoomVcam.enabled = false;
+
+        CanInteract= true;
+
+        yield return null;
+    }
+
+
+
+        IEnumerator MousePress()
+        {
+            while(!Input.GetMouseButtonDown(0))
+            {
+                yield return null;
+            }
+        }
+
+
+
+
+
+
 }
---
+++ Assets/Scripts/LookAtFunction.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtFunction : MonoBehaviour
+{
+public Animator animator;
+
+public bool IKActive = false;
+
+public Transform LookAtObj = null; 
+
+public float LookWeight = 2f;
+
+private void OnAnimatorIK(int layerIndex)
+{
+    if (animator)
+    {
+        if(IKActive)
+        {
+            if (LookAtObj != null)
+            {
+                LookWeight = Mathf.Lerp(LookWeight, 1, Time.deltaTime * 2);
+
+                
+            }
+        }
+        else
+        {
+            LookWeight = Mathf.Lerp(LookWeight, 0, Time.deltaTime * 2);
+         
+        }
+
+                animator.SetLookAtWeight(LookWeight);
+                animator.SetLookAtPosition(LookAtObj.position);
+
+
+    }
+}
+}
