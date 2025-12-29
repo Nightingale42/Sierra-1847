9c4cd33b2bef29db13d4c2590e8f482e99aa7d03
Nightingale42 <89876698+Nightingale42@users.noreply.github.com>
2025-12-28 19:03:59 -0500

    campsite 2 logic
    set up the base logic for campsite 2 foraging scene and goods appraisal

--- Assets/Scripts/D2Caminteract.cs
+++ Assets/Scripts/D2Caminteract.cs
@@ -1,48 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Cinemachine;
 
 public class D2Caminteract : MonoBehaviour
 {
-    // ---------------- INTERACTION ----------------
     public bool TalkToActualFriend = false;
     public bool TalkToRedFriend = false;
+
     public LookAt3 LookAtScript;
     public Text InteractionText;
-    private float InteractDistance = 2f;
+    public float InteractDistance = 2f;
     public bool CanInteract = true;
 
-    // ---------------- PROGRESSION ----------------
     public static int count;
 
-    // ---------------- CAMERAS ----------------
     public CinemachineVirtualCamera PlayerVcam;
     public CinemachineVirtualCamera TalkZoomVcam;
     public CinemachineVirtualCamera RedFriendZoomVcam;
     public NewPlayerMovement FpsController;
 
-    // ---------------- UI ----------------
     public GameObject TalkPanel;
     public GameObject ChoicePack;
+
+    public GameObject ForagedGoodsBowl;
+    public AudioClip BowlSpawnSound;
+
+    private AudioSource audioSource;
+
     public Text SubText;
 
     string holder;
     float time = 0.05f;
 
-    // ---------------- SLEEP SYSTEM ----------------
     private SleepSystem sleepSystem;
-    private bool sleepEnabled = false;
 
     void Start()
     {
         sleepSystem = GetComponent<SleepSystem>();
+
+        // Ensure we have an AudioSource
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
     }
 
-    // ------------------------------------------
-    // UPDATE
-    // ------------------------------------------
     void Update()
     {
         if (!CanInteract)
@@ -51,21 +53,27 @@ public class D2Caminteract : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, InteractDistance))
+        if (!Physics.Raycast(ray, out hit, InteractDistance))
         {
-            if (hit.collider.CompareTag("Friend"))
-            {
-                InteractionText.text = "Talk To Him";
+            InteractionText.text = "";
+            return;
+        }
 
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    CanInteract = false;
-                    StartCoroutine(TalkToFriendCO());
-                }
-            }
-            else if (hit.collider.CompareTag("RedFriend"))
+        if (hit.collider.CompareTag("Friend"))
+        {
+            InteractionText.text = "Talk To Him";
+
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                InteractionText.text = "Talk to Red Friend";
+                CanInteract = false;
+                StartCoroutine(TalkToFriendCO());
+            }
+        }
+        else if (hit.collider.CompareTag("RedFriend"))
+        {
+            if (count > 5)
+            {
+                InteractionText.text = "Talk to Diana";
 
                 if (Input.GetKeyDown(KeyCode.E))
                 {
@@ -75,7 +83,7 @@ public class D2Caminteract : MonoBehaviour
             }
             else
             {
-                InteractionText.text = "";
+                InteractionText.text = "They don't seem interested.";
             }
         }
         else
@@ -84,14 +92,19 @@ public class D2Caminteract : MonoBehaviour
         }
     }
 
-    // ------------------------------------------
-    // RED FRIEND
-    // ------------------------------------------
+    // ================= RED FRIEND =================
     IEnumerator TalkToRedFriendCO()
     {
         TalkToRedFriend = true;
         InteractionText.text = "";
         FpsController.enabled = false;
+
+        // Spawn bowl + play sound
+        if (ForagedGoodsBowl != null)
+            ForagedGoodsBowl.SetActive(true);
+
+        if (audioSource != null && BowlSpawnSound != null)
+            audioSource.PlayOneShot(BowlSpawnSound);
 
         RedFriendZoomVcam.enabled = true;
         PlayerVcam.enabled = false;
@@ -104,7 +117,7 @@ public class D2Caminteract : MonoBehaviour
         TalkPanel.SetActive(true);
 
         SubText.text = "Me: ";
-        holder = "How're you holding up kid?";
+        holder = "Hey there. I just picked these out in the woods, do you know if they're safe to eat?";
         foreach (char c in holder)
         {
             SubText.text += c;
@@ -113,8 +126,42 @@ public class D2Caminteract : MonoBehaviour
 
         yield return MousePress();
 
-        SubText.text = "Kid: ";
-        holder = "Not great I guess... It's so cold out and my stomach hurts. I haven't eaten in days.";
+        SubText.text = "Diana: ";
+        holder = "That mushroom is an Amnita, it's pretty poisonous so I wouldn't recomend eating it. Honestly I wouldn't recommend eating anything in that basket thats touched it, but we both know out situation is too dire for percautions.";
+        foreach (char c in holder)
+        {
+            SubText.text += c;
+            yield return new WaitForSeconds(time);
+        }
+
+        yield return MousePress();
+
+
+        
+        SubText.text = "Me: ";
+        holder = "Okay, so basically everything but the mushroom is fine?";
+        foreach (char c in holder)
+        {
+            SubText.text += c;
+            yield return new WaitForSeconds(time);
+        }
+
+        yield return MousePress();
+
+
+         SubText.text = "Diana: ";
+        holder = "Relatively.";
+        foreach (char c in holder)
+        {
+            SubText.text += c;
+            yield return new WaitForSeconds(time);
+        }
+
+        yield return MousePress();
+
+
+        SubText.text = "Me: ";
+        holder = "Good enough for me.";
         foreach (char c in holder)
         {
             SubText.text += c;
@@ -126,9 +173,7 @@ public class D2Caminteract : MonoBehaviour
         StartCoroutine(FinalCO());
     }
 
-    // ------------------------------------------
-    // MAIN FRIEND DIALOGUE
-    // ------------------------------------------
+    // ================= MAIN FRIEND =================
     IEnumerator TalkToFriendCO()
     {
         TalkToActualFriend = true;
@@ -148,19 +193,13 @@ public class D2Caminteract : MonoBehaviour
 
         TalkPanel.SetActive(true);
 
-        // ---------- PRIORITY CHECKS ----------
-
-        if (EatingSystem.SliceEaten > 2)
+        if (D2EatingSystem.SliceEaten > 1)
         {
-            if (!sleepEnabled)
-            {
-                sleepEnabled = true;
-                if (sleepSystem != null)
-                    sleepSystem.enabled = true;
-            }
+            if (sleepSystem != null)
+                sleepSystem.enabled = true;
 
             SubText.text = "Friend: ";
-            holder = "Looks like you got some dinner. Probably best to head to bed, we've got a big day ahead.";
+            holder = "So how were they? Edible?";
             foreach (char c in holder)
             {
                 SubText.text += c;
@@ -175,7 +214,7 @@ public class D2Caminteract : MonoBehaviour
         if (count > 2)
         {
             SubText.text = "Friend: ";
-            holder = "Thanks for collecting those logs. There's some soup in the communal pot. Don't ask what's in it.";
+            holder = "Oh nice! You got some stuff. I can't really tell what's safe or not... Oh, I know! You should go talk to Diana. She's able to recognize lots of forageables.";
             foreach (char c in holder)
             {
                 SubText.text += c;
@@ -187,7 +226,6 @@ public class D2Caminteract : MonoBehaviour
             yield break;
         }
 
-        // ---------- DEFAULT DIALOGUE ----------
         SubText.text = "Friend: ";
         holder = "I'm real hungry. Damn Marison keeping all the food to himself.";
         foreach (char c in holder)
@@ -220,9 +258,7 @@ public class D2Caminteract : MonoBehaviour
         ChoicePack.SetActive(true);
     }
 
-    // ------------------------------------------
-    // CHOICES
-    // ------------------------------------------
+    // ================= CHOICES =================
     public void Choice1Void() => StartCoroutine(Choice1CO());
     public void Choice2Void() => StartCoroutine(Choice2CO());
 
@@ -268,13 +304,10 @@ public class D2Caminteract : MonoBehaviour
         StartCoroutine(FinalCO());
     }
 
-    // ------------------------------------------
-    // RESET
-    // ------------------------------------------
     IEnumerator FinalCO()
     {
-        TalkToRedFriend = false;
         TalkToActualFriend = false;
+        TalkToRedFriend = false;
 
         TalkPanel.SetActive(false);
         ChoicePack.SetActive(false);
